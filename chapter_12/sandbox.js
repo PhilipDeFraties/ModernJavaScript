10ceb2857d5bd6c799621d4ce284fc2826fb2774
Philip DeFraties <65036872+PhilipDeFraties@users.noreply.github.com>
2022-08-08 22:59:22 -0600

    finished chapter 12

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,25 +1,15 @@
-const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = async () => {
+    const response = await fetch('todos/phil.json');
 
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if(request.readyState === 4){
-            callback('could not fetch data', undefined);
-        }
-    });
-    
-    request.open('GET', "todos.json");
-    request.send();
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
+
+    const data = await response.json();
+    return data;
+
 };
 
-
-getTodos((err, data) => {
-    console.log('callback fired');
-    if(err){
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+getTodos()
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
